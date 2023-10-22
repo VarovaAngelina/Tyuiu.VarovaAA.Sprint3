dcfd473e0667b7f1bd7d9f627dfb01acd7ada813
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-10-22 23:58:26 +0500

    Task2

---
+++ Tyuiu.VarovaAA.Sprint3.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VarovaAA.Sprint3.Task2.V3.Lib;
+namespace Tyuiu.VarovaAA.Sprint3.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 4;
+            int startValue = 1;
+            int stopValue = 15;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 1935;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
